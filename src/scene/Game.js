bb60f5a7254bbbeda17c64bea1fda87934543ae4
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2024-03-25 23:15:45 -0300

    mygame

---
+++ src/scene/Game.js
@@ -0,0 +1,40 @@
+import Phaser from '../lib/phaser.js'
+
+ export default class Game extends Phaser.Scene{
+ 
+    constructor(){
+
+    super('game'); 
+    }
+
+    preload(){
+        this.load.image("background","./src/assets/bg_layer1.png"); 
+        this.load.image("platform",  "./src/assets/ground_grass.png"); 
+        this.load.image("bunny-stand", "./src/assets/bunny1_stand.png"); 
+        
+    }  
+    create(){
+        this.add.image(240, 320, 'background'); 
+        //
+        //vai criar um grupo de plataforma
+        const platforms =  this.physics.add.staticGroup(); 
+        for(let i = 0 ; i < 5; i++){
+            const x = Phaser.Math.Between(80,400); 
+            const y = 150*i; 
+            
+            const platform = platforms.create(x, y , 'platform'); 
+            //vai separar as plataformas 
+            platform.scale= 0.5;
+            const body = platform.body; 
+            body.updateFromGameObject(); 
+        }
+        //criar o jogador
+        const player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
+       // colidir com a plataforma
+       this.physics.add.collider(platforms, player); 
+    }
+
+    update(){
+  
+    }
+}
